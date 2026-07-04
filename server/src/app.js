db2478efac3c943628a1e948bd3d4e85beca8a57
Mohammed Kaif <mohamkaif2003@gmail.com>
2026-07-04 19:40:00 +0530

    Day 14

--- server/src/app.js
+++ server/src/app.js
@@ -16,8 +16,19 @@ dotenv.config()
 
 const app = express();
 
+const allowedOrigins = [
+    "http://localhost:3000",
+    process.env.FRONTEND_URL,
+];
+
 app.use(cors({
-        origin: "http://localhost:3000",
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
         credentials: true,
     }));
 app.use(express.json());
